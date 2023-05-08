ad9744ddba48108d9b5164f705c0709babbf59b1
fordonez792 <fordonez792@gmail.com>
2023-05-09 01:33:23 +0800

    Fixed Bugs

--- server/models/Reviews.js
+++ server/models/Reviews.js
@@ -1,55 +1,62 @@
 module.exports = (sequelize, DataTypes) => {
   // Creates a table with the given columns in database
-  const Reviews = sequelize.define("Reviews", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const Reviews = sequelize.define(
+    "Reviews",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      semester: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      engaging: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      effectiveness: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      fairAssessments: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      recommend: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      numberOfHelpfulVotes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      numberOfReportVotes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      anonymous: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    year: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    semester: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    difficulty: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    engaging: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    effectiveness: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    fairAssessments: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    recommend: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    numberOfHelpfulVotes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-    numberOfReportVotes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-    anonymous: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  });
+    {
+      charset: "utf8mb4",
+      collate: "utf8mb4_unicode_ci",
+    }
+  );
 
   // Connects table to users, courses, helpfulvotes, and reportvotes tables
   Reviews.associate = (models) => {
